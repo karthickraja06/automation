f9352fe298da96186887bcde072b9d4ff37866f5
Karthick Raja <karthickrajab03@gmail.com>
2025-02-23 22:48:07 +0530

    Merging of the 4 stages have been updated

--- automation.py
+++ automation.py
@@ -1,8 +1,14 @@
 # automation.py
 import datetime
-
+from modules.organize import update_google_sheets
+from modules.audio_generation import generate_speech  
 def run_task():
     print(f"Automation running at {datetime.datetime.now()}")
+    update_google_sheets()
+    print("Script completed.")
+    generate_speech()
+    print("Voice generate completed.")
+
 
 if __name__ == "__main__":
     run_task()
